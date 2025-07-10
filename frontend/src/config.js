846bd747d7b5f365ad4196bf32ff8a8b11729bab
Ajeet Kumar <ajeet62068bca@gmail.com>
2025-07-10 14:27:48 +0530

    fixed 404

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 const config = {
-    backendUrl: "https://paytm-629n.onrender.com", // Replace with your actual backend URL
+    backendUrl: "http://localhost:3000", // Replace with your actual backend URL
   };
   
 export default config;
